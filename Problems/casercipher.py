7d000d705ee924fb0a2ced9d239cf958b6b90049
Sri-Aryan-sudo <aryanthecoder18@gmail.com>
2025-07-24 22:35:28 +0530

    Casercipher Solution

--- Problems/casercipher.py
+++ Problems/casercipher.py
@@ -1,27 +1,30 @@
-def enconding(word,shift):
+def encoding(word,shift):
     encoded_word=""
     for char in word:
+        shift_amount=shift%26
         if char.isalpha():
-            shift_amount = shift % 26
-            if char.islower():
-                base=ord('a')
-            elif char.isupper():
+            base=0
+            if char.isupper():
                 base=ord('A')
+            else:
+                base=ord('a')
             encoded_word+=chr((ord(char)-base+shift_amount)%26+base)
         else:
             encoded_word+=char
-      return encoded_word
-
+    return encoded_word
 def decoding(word,shift):
     decoded_word=""
     for char in word:
+        shift_amount=shift%26
         if char.isalpha():
-            shift_amount = shift % 26
-            if char.islower():
-                base=ord('a')
-            elif char.isupper():
+            base=0
+            if char.isupper():
                 base=ord('A')
+            else:
+                base=ord('a')
             decoded_word+=chr((ord(char)-base-shift_amount)%26+base)
         else:
             decoded_word+=char
     return decoded_word
+word='bszbo'
+print(decoding(word,1))
